04934e44cbe64e064a1124e2903f160e5f1294d1
Elvio Galeano <elvio.galeano827@gmail.com>
2022-11-18 18:29:37 -0300

    instalacion de express, mongo

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(
+	process.env.MONGO_CONNECTION,
+{
+	useUnifiedTopology: true,
+	useNewUrlParser: true
+}
+	).then(()=> console.log('conectado a la base de datos'))
+	.catch(error => console.log(error))
